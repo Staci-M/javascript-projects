c8691d306f74e20aa24899b43e9c9b766820e878
Staci McDade <stacimcdade@Stacis-MBP.lan>
2023-09-13 09:39:06 -0500

    <exercises completed 9/13/23>

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -3,19 +3,20 @@
 // Refactor the code to do this. Verify that your change works by updating the console.log statements.
 
 let launchReady = false;
+let fuelReady = false;
 let fuelLevel = 17000;
 let crewStatus = true;
 let computerStatus = 'green';
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
-   launchReady = true;
+   fuelReady = true;
 } else {
    console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
+   fuelReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+//console.log("launchReady = ", launchReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
@@ -25,4 +26,10 @@ if (crewStatus && computerStatus === 'green'){
    launchReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+//console.log(`launchReady = ${launchReady} and fuelReady = ${fuelReady}`);
+
+if(launchReady && fuelReady){
+   console.log('9, 8, 7, 6, 5, 4, 3, 2, 1... LIFTOFF!');
+}else{
+   console.log('Launch Scrubbed');
+}
