90dcf4a21753c452ce7d831c5f6e4d1dc987fd95
Staci McDade <stacimcdade@Stacis-MBP.lan>
2023-10-04 17:17:05 -0500

    <complete>

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -5,11 +5,42 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
+let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+
+let trippledValue = function(value){
+    if( typeof value === 'number'){
+        value = value * 3;
+        return value;
+    } else if(typeof value === 'string'){
+        return 'ARRR!';
+    } else {
+        return value;
+    }
+
+};
+
+let trippledValueMapped = arr.map(trippledValue);
+
+console.log(trippledValueMapped);
+
+console.log(trippledValue(true));
 
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
+let mappedArray = arr.map(function (value) {
+    if (typeof value === 'number') {
+        return value * 3;
+    } else if (typeof value === 'string') {
+        return 'ARRR!';
+    } else {
+        return value;
+    }
+});
 
-let arr = ['Elocution', 21, 'Clean teeth', 100];
+console.log(mappedArray);
+console.log(arr);
+
--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -18,14 +18,19 @@ function holdStatus(arr){
   }
 }
 
+
+
 let fuelLevel = 200000;
 let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold', 'water', 'AE-35 unit'];
 
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
 
-/* Steal some fuel from the shuttle:
- * /
+//Steal some fuel from the shuttle:
+ 
+let optimizePotential = checkFuel(fuelLevel);
+
+console.log(optimizePotential);
  
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
 
@@ -35,23 +40,63 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //d). Decide where to best place your function call to gather our new fuel.
 
+let optimizer = function(optimizePotential){
+  let optimizationResults = fuelLevel;
+  if(optimizePotential === 'green'){
+    optimizationResults -= 100001;
+    fuelLevel -= optimizationResults
+    return optimizationResults;
+  } else if (optimizePotential === 'yellow'){
+    optimizationResults - 50001;
+    fuelLevel -= optimizationResults
+    return optimizationResults;
+  } else {
+    fuelLevel -= optimizationResults
+    return optimizationResults;
+  }
+};
+// console.log(optimizer(optimizePotential));
+// console.log(fuelLevel);
 /* Next, liberate some of that glorious cargo.
- * /
+ */
+
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
-//b). You need to swipe two items from the cargo hold. Choose well. Stealing water ain’t gonna get us rich. Put the swag into a new array and return it from the function.
+let orderItems = function(inventory){
+  let order = inventory.splice(inventory.indexOf('gold'), 1, 'brick');
+  item2 = inventory.splice(inventory.indexOf('AE-35 unit'), 1, 'cargo box')
+  order.push(item2[0]);
+  return order
 
-//c). The cargo hold has better security than the fuel tanks. It counts how many things are in storage. You need to replace what you steal with something worthless. The count MUST stay the same, or you’ll get caught and thrown into the LaunchCode brig.
+};
 
-//d). Don’t get hasty, matey! Remember to test your function.
+// console.log(orderItems(cargoHold));
+// console.log(cargoHold);
+// //b). You need to swipe two items from the cargo hold. Choose well. Stealing water ain’t gonna get us rich. Put the swag into a new array and return it from the function.
 
-/* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
- * /
+// //c). The cargo hold has better security than the fuel tanks. It counts how many things are in storage. You need to replace what you steal with something worthless. The count MUST stay the same, or you’ll get caught and thrown into the LaunchCode brig.
+
+// //d). Don’t get hasty, matey! Remember to test your function.
+
+// /* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
+//  * /
  
-//a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
-	
-//b). Call your anonymous fuel and cargo functions from within irs.
+// //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
+function irs(fuelLevel, cargoHold) {
+  let fuelReceipt = optimizer(checkFuel(fuelLevel));
+  let itemReciept = orderItems(cargoHold);
 
-//c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
+  return `Raided ${fuelReceipt} kg of fuel from the tanks, and stole ${itemReciept[0]} and ${itemReciept[1]} from the cargo hold.`;
+}
 
+console.log(irs(fuelLevel, cargoHold));
+// let irs = function(optimizer) {
+//   let order = optimizer;
+//   return order
+//   // return`Raided ${fuelReceipt} kg of fuel from the tanks, and stole ${cargoRecipt[0]} and ${cargoRecipt[1]} from the cargo hold.`
+// }
+// 	console.log(irs(optimizer));
+// //b). Call your anonymous fuel and cargo functions from within irs.
+
+// //c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
